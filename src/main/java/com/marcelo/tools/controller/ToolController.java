b011cae5d9b768ad51402b12be858687f688e44d
marcelosdm <marcelosdm@gmail.com>
2019-12-20 12:34:16 -0300

    Allow CORS policy

--- src/main/java/com/marcelo/tools/controller/ToolController.java
+++ src/main/java/com/marcelo/tools/controller/ToolController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.marcelo.tools.entity.Tool;
 import com.marcelo.tools.error.NotFoundError;
 import com.marcelo.tools.repository.ToolRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/tools")
 public class ToolController {
